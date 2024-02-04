c4e0b40266e2b84eead6cd6d9c01d1b3c1e47626
Fugazzii <sichinavailia@gmail.com>
2024-02-04 14:26:43 +0400

    Implemented client for solana

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use solana_client::rpc_client;
-
 mod database;
 mod json_rpc_client;
 
 use database::{Database, Event};
-
+use json_rpc_client::SolanaClient;
 
 const JSON_RPC_URL: &str = "https://api.devnet.solana.com/";
 
@@ -12,8 +10,7 @@ const JSON_RPC_URL: &str = "https://api.devnet.solana.com/";
 async fn main() {
     let _db: Database<Event> = Database::new();
 
-    let client = rpc_client::RpcClient::new(JSON_RPC_URL);
+    let client = SolanaClient::new(JSON_RPC_URL);
 
-    let recent_blockhash = client.get_latest_blockhash().unwrap();
-    println!("Recent blockhash: {:?}", recent_blockhash);
+    client.get_latest_hash();
 }
