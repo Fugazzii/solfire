0b39c0597dd570541430985d452c2da89c06c7d7
Fugazzii <sichinavailia@gmail.com>
2024-02-04 19:14:13 +0400

    Finished creating account feature

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
-use solana_program::{
-    instruction::Instruction,
-    pubkey::Pubkey
-};
+use solana_program::{instruction::Instruction, pubkey::Pubkey};
 
-use solana_sdk::{
-    signature::read_keypair_file, signer::Signer, transaction::Transaction
-};
+use solana_sdk::{signature::read_keypair_file, signer::Signer, transaction::Transaction};
 
 mod solana_rpc_client;
 use solana_rpc_client::SolanaClient;
 
-const RPC_DEVNET_URL: &str = "https://api.devnet.solana.com/";
+const DEVNET_RPC_URL: &str = "https://api.devnet.solana.com/";
+// const TESTNET_RPC_URL: &str = "https://api.testnet.solana.com/";
+// const MAINNET_RPC_URL: &str = "https://api.mainnet.solana.com/";
 
 #[tokio::main]
-async fn main() {    
-
+async fn main() {
     let system_id: Pubkey = Pubkey::default();
 
-    let client = SolanaClient::new(RPC_DEVNET_URL);
+    let client = SolanaClient::connect(DEVNET_RPC_URL);
 
     let keypair1 = read_keypair_file("./wallets/w1.json").unwrap();
     let keypair2 = read_keypair_file("./wallets/w2.json").unwrap();
@@ -25,29 +21,32 @@ async fn main() {
     let (pubkey1, pubkey2) = (keypair1.pubkey(), keypair2.pubkey());
     let (sender, recipient) = (
         client.describe_account(&pubkey1, true),
-        client.describe_account(&pubkey2, false)
+        client.describe_account(&pubkey2, false),
     );
 
     // 42 lamports
-    let instruction_data = &[2,0,0,0,42,0,0,0,0,0,0,0];
+    let instruction_data = &[2, 0, 0, 0, 42, 0, 0, 0, 0, 0, 0, 0];
 
-    let ix = Instruction::new_with_bytes(
-        system_id,
-        instruction_data,
-        vec![sender, recipient]
-    );
-    
+    let ix = Instruction::new_with_bytes(system_id, instruction_data, vec![sender, recipient]);
+
     let signers = [&keypair1];
 
     let tx = Transaction::new_signed_with_payer(
-        &[ix], 
-        Some(&pubkey1), 
-        &signers, 
-        client.get_latest_hash()
+        &[ix],
+        Some(&pubkey1),
+        &signers,
+        client.get_latest_hash(),
     );
 
     let sig = client.send_tx(&tx);
 
-    println!("Signature: {}", sig);
+    /*
+        let signature = client.perform_tx(
+            signer: Keypair,
+            recipient_pubkey: PubKey,
+            sols: u32
+        );
+    */
 
+    println!("Signature: {}", sig);
 }
--- src/solana_rpc_client.rs
+++ src/solana_rpc_client.rs
@@ -1,83 +1,133 @@
+#[allow(unused_imports)]
 use rand_core;
+
+#[allow(unused_imports)]
 use solana_program::{
-	hash::Hash, instruction::AccountMeta, native_token::{sol_to_lamports, LAMPORTS_PER_SOL}, pubkey::Pubkey, system_program
+    hash::Hash,
+    instruction::AccountMeta,
+    native_token::{sol_to_lamports, LAMPORTS_PER_SOL},
+    pubkey::Pubkey,
+    system_program,
 };
-use solana_client::{client_error::ClientError, rpc_client::RpcClient, rpc_config::RpcSendTransactionConfig};
+
+#[allow(unused_imports)]
+use solana_client::{
+    client_error::ClientError, rpc_client::RpcClient, rpc_config::RpcSendTransactionConfig,
+};
+
+use solana_sdk::signature::{read_keypair_file, write_keypair_file};
+#[allow(unused_imports)]
 use solana_sdk::{
-	signature::{Keypair, Signature}, signer::Signer, system_instruction::create_account, transaction::Transaction
+    signature::{Keypair, Signature},
+    signer::Signer,
+    system_instruction::create_account,
+    transaction::Transaction,
 };
 
 pub struct SolanaClient {
-	client: RpcClient
+    client: RpcClient,
 }
 
+#[allow(dead_code)]
 impl SolanaClient {
+    pub fn connect(url: &str) -> Self {
+        SolanaClient {
+            client: RpcClient::new(url),
+        }
+    }
 
-	pub fn new(url: &str) -> Self {
-		SolanaClient {
-			client: RpcClient::new(url) 
-		}
-	}
+    pub fn read_keypair(&self, file_path: &str) -> Keypair {
+        match read_keypair_file(file_path) {
+            Ok(keypair) => keypair,
+            Err(err) => panic!("Failed to read keypair file. {:?}", err.to_string()),
+        }
+    }
 
-	pub fn send_tx(&self, tx: &Transaction) -> Signature {
-		match self.client.send_transaction(	tx) {
-			Ok(sig) => sig,
-			Err(err) => self.handle_err(&err)
-		}
-	}
+    pub fn perform_tx(&self, signer: &Keypair, recipient_pubkey: &Pubkey, sols: u32) -> Signature {
+        unimplemented!()
+    }
 
-	pub fn send_tx_without_preflight(&self, tx: &Transaction) -> Signature {
-	    let mut config: RpcSendTransactionConfig = RpcSendTransactionConfig::default();
-		config.skip_preflight = true;
-		
-		match self.client.send_transaction_with_config(	tx, config) {
-			Ok(sig) => sig,
-			Err(err) => self.handle_err(&err)
-		}
-	}
+    pub fn send_tx(&self, tx: &Transaction) -> Signature {
+        match self.client.send_transaction(tx) {
+            Ok(sig) => sig,
+            Err(err) => {
+                let e = err.get_transaction_error().unwrap();
+                panic!("Failed to send tx. {:?}", e)
+            }
+        }
+    }
 
-	fn handle_err(&self, err: &ClientError) -> Signature {
-		let e = err.get_transaction_error().unwrap();
-		panic!("Transaction failed. {:?}", e)
-	}
+    pub fn send_tx_without_preflight(&self, tx: &Transaction) -> Signature {
+        let mut config: RpcSendTransactionConfig = RpcSendTransactionConfig::default();
+        config.skip_preflight = true;
 
-	pub fn get_latest_hash(&self) -> Hash {
-		let c: Hash = self.client.get_latest_blockhash().unwrap();
-		println!("Recent blockhash: {:?}", c);
-		c
-	}
+        match self.client.send_transaction_with_config(tx, config) {
+            Ok(sig) => sig,
+            Err(err) => {
+                let e = err.get_transaction_error().unwrap();
+                panic!("Failed to send tx. {:?}", e)
+            }
+        }
+    }
 
-	pub fn airdrop(&self, pubkey: &Pubkey, sols: u32) -> Signature {
-		let lamports = sol_to_lamports(sols as f64);
-		match self.client.request_airdrop(pubkey, lamports) {
-			Ok(sig) => {
-				println!("Airdropped 1 SOL\nSignature: {}", sig);
-				sig
-			}
-			Err(err) => panic!("Failed to airdrop. {:?}", err)
-		}
-	}
+    pub fn get_latest_hash(&self) -> Hash {
+        let c: Hash = self.client.get_latest_blockhash().unwrap();
+        println!("Recent blockhash: {:?}", c);
+        c
+    }
 
-	pub fn describe_account(&self, pubkey: &Pubkey, is_signer: bool) -> AccountMeta {
-		AccountMeta {
-			pubkey: *pubkey,
-			is_signer,
-			is_writable: true 
-		}
-	}
+    pub fn airdrop(&self, pubkey: &Pubkey, sols: u32) -> Signature {
+        let lamports = sol_to_lamports(sols as f64);
+        match self.client.request_airdrop(pubkey, lamports) {
+            Ok(sig) => {
+                println!("Airdropped 1 SOL\nSignature: {}", sig);
+                sig
+            }
+            Err(err) => panic!("Failed to airdrop. {:?}", err),
+        }
+    }
 
-	// pub fn create_wallet(&self, passphrase: &str) {
-	// 	let keypair = Keypair::generate(&mut rand_core::OsRng);
-	// 	let recent_blockhash = self.get_latest_hash();
-	// 	let instructions = create_account(
-	// 		&system_program::id(),
-	// 		&keypair.pubkey(),
-    //         LAMPORTS_PER_SOL,
-	// 		1024,
-	// 		&keypair.pubkey()
-	// 	);
+    pub fn describe_account(&self, pubkey: &Pubkey, is_signer: bool) -> AccountMeta {
+        AccountMeta {
+            pubkey: *pubkey,
+            is_signer,
+            is_writable: true,
+        }
+    }
 
-	// }
+    pub fn create_wallet(&self, passphrase: &str, file_name: &str) {
+        let keypair = Keypair::from_bytes(passphrase.as_bytes()).unwrap_or(
+            // Default keypair
+            Keypair::generate(&mut rand_core::OsRng),
+        );
+        let recent_blockhash = self.get_latest_hash();
+        let lamports = sol_to_lamports(1.0);
+        let space = 1024;
+        write_keypair_file(&keypair, format!("./wallets/{}.json", file_name)).unwrap();
 
+        let create_account_instruction = create_account(
+            &keypair.pubkey(),
+            &keypair.pubkey(),
+            lamports,
+            space,
+            &system_program::id(),
+        );
 
+        let transaction = Transaction::new_signed_with_payer(
+            &[create_account_instruction],
+            Some(&keypair.pubkey()),
+            &[&keypair],
+            recent_blockhash,
+        );
+
+        let result = self.client.send_and_confirm_transaction(&transaction);
+        match result {
+            Ok(signature) => {
+                println!("Transaction confirmed. Signature: {:?}", signature);
+            }
+            Err(err) => {
+                println!("Transaction failed. Error: {:?}", err);
+            }
+        }
+    }
 }
